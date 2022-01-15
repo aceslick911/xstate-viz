bb6b8b6297fa2667dd5a0838a3f706f7bd96ecb3
Angelo Perera <angelo.perera@betfair.com.au>
2022-01-15 16:51:55 +1100

    https

--- src/simulationMachine.tsx
+++ src/simulationMachine.tsx
@@ -105,7 +105,7 @@ export const simulationMachine = simModel.createMachine(
                 const receiver = createWebSocketReceiver({
                   //  server: 'localhost:8888',
                   server: 'xstate-viz-socks.herokuapp.com',
-                  //  protocol: 'ws',
+                  protocol: 'wss',
                 });
 
                 onReceive((event) => {
